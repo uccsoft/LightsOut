30393c5a45a5cd9c521c44c440cfd0ec667e5099
umitcan.cukadar <umitcan.cukadar@gmail.com>
2021-11-20 15:28:26 +0300

    Created GameSettings model.

---
+++ src/LightsOut.Api/Models/GameSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LightsOut.Api.Models
+{
+    public class GameSettings
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public int BoardSize { get; set; }
+    }
+}
