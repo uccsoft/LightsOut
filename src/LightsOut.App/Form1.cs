fa1d239bc14a01318c23472b16b5cf940b00f41e
umitcan.cukadar <umitcan.cukadar@gmail.com>
2021-11-21 18:30:10 +0300

    Getting game settings from the api.

---
+++ src/LightsOut.App/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LightsOut.App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            LightsOutApiClient lightsOutApiClient = new LightsOutApiClient("localhost", "5000");
+            var gameSettings = await lightsOutApiClient.GetGameSettings(1);
+        }
+    }
+}
---
+++ src/LightsOut.App/LightsOutApiClient.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using LightsOut.Api.Models;
+
+namespace LightsOut.App
+{
+    class LightsOutApiClient
+    {
+        private readonly Uri _baseUri;
+
+        public LightsOutApiClient(string host, string port)
+        {
+            _baseUri = new Uri($"http://{host}:{port}");
+        }
+
+        private async Task<string> GetRawDataFromApi(string relativeUrl)
+        {
+            using (var client = new HttpClient())
+            {
+                using (var result = await client.GetAsync($"{_baseUri}{relativeUrl}"))
+                {
+                    result.EnsureSuccessStatusCode();
+                    return await result.Content.ReadAsStringAsync();
+                }
+            }
+        }
+
+        public async Task<GameSettings> GetGameSettings(int id)
+        {
+            var rawData = await GetRawDataFromApi($"api/gamesettings/{id}");
+
+            return JsonSerializer.Deserialize<GameSettings>(rawData, new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            }); 
+        }
+    }
+}
