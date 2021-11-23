acd8e4cc8dc1b8efcac0d061f65655916eb9cb81
umitcan.cukadar <umitcan.cukadar@gmail.com>
2021-11-23 11:08:45 +0300

    Deleted unsed code from project.

--- src/LightsOut.Api/Controllers/GameSettingsController.cs
+++ src/LightsOut.Api/Controllers/GameSettingsController.cs
@@ -21,13 +21,6 @@ namespace LightsOut.Api.Controllers
             _context = context;
         }
 
-        // GET: api/GameSettings
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<GameSettings>>> GetGameSettings()
-        {
-            return await _context.GameSettings.ToListAsync();
-        }
-
         // GET: api/GameSettings/5
         [HttpGet("{id}")]
         public async Task<ActionResult<GameSettings>> GetGameSettings(int id)
@@ -56,71 +49,6 @@ namespace LightsOut.Api.Controllers
             #endregion
 
             return gameSettings;
-        }
-
-        // PUT: api/GameSettings/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutGameSettings(int id, GameSettings gameSettings)
-        {
-            if (id != gameSettings.Id)
-            {
-                return BadRequest();
-            }
-
-            _context.Entry(gameSettings).State = EntityState.Modified;
-
-            try
-            {
-                await _context.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!GameSettingsExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return NoContent();
-        }
-
-        // POST: api/GameSettings
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
-        // more details see https://aka.ms/RazorPagesCRUD.
-        [HttpPost]
-        public async Task<ActionResult<GameSettings>> PostGameSettings(GameSettings gameSettings)
-        {
-            _context.GameSettings.Add(gameSettings);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetGameSettings", new { id = gameSettings.Id }, gameSettings);
-        }
-
-        // DELETE: api/GameSettings/5
-        [HttpDelete("{id}")]
-        public async Task<ActionResult<GameSettings>> DeleteGameSettings(int id)
-        {
-            var gameSettings = await _context.GameSettings.FindAsync(id);
-            if (gameSettings == null)
-            {
-                return NotFound();
-            }
-
-            _context.GameSettings.Remove(gameSettings);
-            await _context.SaveChangesAsync();
-
-            return gameSettings;
-        }
-
-        private bool GameSettingsExists(int id)
-        {
-            return _context.GameSettings.Any(e => e.Id == id);
         }
     }
 }
